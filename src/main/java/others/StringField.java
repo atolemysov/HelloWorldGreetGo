4d52e5f82f1d4c0e444725854328025f9bab8b13
atolemysov <40075230+atolemysov@users.noreply.github.com>
2018-06-09 04:12:01 +0600

    Initial commit

---
+++ src/main/java/others/StringField.java
@@ -0,0 +1,22 @@
+package others;
+
+public class StringField {
+
+    private String str;
+
+    public StringField(String str) {
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public String toString() {
+        return str;
+    }
+}
